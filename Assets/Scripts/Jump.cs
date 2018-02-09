9c353d5cc3e066a85747df6476ce5ebe5c67ea29
Emma Hove Jørgensen <emma@Emmas-MacBook-Pro-5.local>
2018-02-10 00:39:18 +0100

    jump

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+	private Rigidbody _playerRigidbody;
+	[SerializeField] private float _jumpForce = 5;
+	private bool canJump = true;
+	void Start ()
+	{
+		_playerRigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		if (Input.GetKey(KeyCode.Space) && canJump)
+		{
+			_playerRigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+			canJump = false;
+		}
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		canJump = true;
+	}
+}
