dbbf210da257f98237d786d359ee428d3d7219d8
Emma Hove Jørgensen <emma@Emmas-MacBook-Pro-5.local>
2018-02-09 23:16:04 +0100

    platform added and dash

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+	[SerializeField] private float _dashForce = 50;
+	private Rigidbody _playerRigidbody;
+
+	private void Start()
+	{
+		_playerRigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		if (Input.GetAxis("Dash") > 0)
+		{
+			Vector3 nearestForward = Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
+			_playerRigidbody.AddForce(nearestForward * _dashForce, ForceMode.Impulse);
+		}
+	}
+}
