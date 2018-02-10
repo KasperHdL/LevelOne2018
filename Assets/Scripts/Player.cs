187798a6ae9140812b81f695407058a05bb59f90
IYavin <itai.yavin@gmail.com>
2018-02-10 11:48:10 +0100

    Cleaned up the Player Script. Removed a few unnecessary boolean expressions, and moved a lot of functionality down into functions to clear up the Update loop.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour {
     public PlayerSettings settings;
     private bool onGround = true;
     private float nextDash;
+    private Vector3 nearestForward;
 
 
     //temp should use team to get remappings
@@ -25,7 +26,8 @@ public class Player : MonoBehaviour {
         groundMask = LayerMask.GetMask("Ground");
 	}
 
-	private void Update(){
+	private void Update()
+    {
 
         if(inputDevice == null){
             InputDevice test = InputManager.ActiveDevice;
@@ -38,61 +40,85 @@ public class Player : MonoBehaviour {
             return;
         }
 
+        nearestForward = Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
         onGround = Physics.Raycast(transform.position, Vector3.down, settings.distanceFromGround, groundMask);
 
-        Vector2 input = inputDevice.LeftStick.Value;
-		Vector3 movementdirection = new Vector3(input.x, 0, input.y);
-
-		movementdirection = movementdirection.normalized;
-
-        if(movementdirection != Vector3.zero){
-            Quaternion lookRot = Quaternion.LookRotation(movementdirection);
-            body.transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * 30);
-            float force = (onGround ? settings.movementForce : settings.airForce);
-            body.AddForce(movementdirection * force, ForceMode.Acceleration);
+        if (inputDevice.LeftStick.Value != Vector2.zero)
+        {
+            Move();
         }
 
         //Jump
-        if(onGround){
-            if(inputDevice.Action1.WasPressed){
-                body.AddForce(Vector3.up * settings.jumpForce, ForceMode.Impulse);
-            }
+        if(inputDevice.Action1.WasPressed)
+        {
+            Jump();
         }
 
-        Vector3 nearestForward = Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
-
         //Dash
-        if(nextDash < Time.time){
-            if(inputDevice.Action2.WasPressed){
-                body.AddForce(nearestForward * settings.dashForce, ForceMode.Impulse);
-                nextDash = Time.time + settings.dashDelay;
-            }
+        if(inputDevice.Action2.WasPressed)
+        {
+            Dash();
         }
 
         //Push
+        if (inputDevice.RightBumper.WasPressed)
+        {
+            Push();
+        }
+	}
 
+    private void Move()
+    {
+        Vector2 input = inputDevice.LeftStick.Value;
+        
+		Vector3 movementdirection = new Vector3(input.x, 0, input.y);
+		movementdirection = movementdirection.normalized;
+
+        Quaternion lookRot = Quaternion.LookRotation(movementdirection);
+        body.transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * 30);
+        float force = (onGround ? settings.movementForce : settings.airForce);
+        body.AddForce(movementdirection * force, ForceMode.Acceleration);
+    }
+
+    private void Jump()
+    {
+        if(onGround)
+        {
+            body.AddForce(Vector3.up * settings.jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void Dash()
+    {
+        if(nextDash < Time.time)
+        {
+            body.AddForce(nearestForward * settings.dashForce, ForceMode.Impulse);
+            nextDash = Time.time + settings.dashDelay;
+        }
+    }
+
+    private void Push()
+    {
         RaycastHit[] hits;
+        hits = Physics.SphereCastAll(transform.position, settings.pushDistance, nearestForward, 0.0001f);
 
-        if (inputDevice.RightBumper.WasPressed){
-            hits = Physics.SphereCastAll(transform.position, settings.pushDistance, nearestForward, 0.0001f);
+        for( int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
 
-            for( int i = 0; i < hits.Length; i++)
+            Rigidbody hitBody = hit.rigidbody;
+
+            if (hitBody && hitBody != body)
             {
-                RaycastHit hit = hits[i];
-
-                Rigidbody hitBody = hit.transform.GetComponent<Rigidbody>();
-
-                if (hitBody && hitBody != body)
+                Vector3 direction = Vector3.ProjectOnPlane(hitBody.transform.position - transform.position, Vector3.up).normalized;
+                float dot = Vector3.Dot(direction, nearestForward);
+                float angle = Mathf.Rad2Deg * Mathf.Acos(dot);
+                
+                if(angle < settings.pushDegrees)
                 {
-                    Vector3 direction = Vector3.ProjectOnPlane(hitBody.transform.position - transform.position, Vector3.up).normalized;
-                    float dot = Vector3.Dot(direction, nearestForward);
-                    float angle = Mathf.Rad2Deg * Mathf.Acos(dot);
-                    
-                    if(angle < settings.pushDegrees && hitBody != body){
-                        hitBody.AddForce(dot * direction * settings.pushForce, ForceMode.Impulse);
-                    }
+                    hitBody.AddForce(dot * direction * settings.pushForce, ForceMode.Impulse);
                 }
             }
         }
-	}
+    }
 }
