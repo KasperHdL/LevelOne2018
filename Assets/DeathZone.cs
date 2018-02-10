d5b202b9964cf79499dcd4f0a2d02e465935339a
Unknown <mdhe@itu.dk>
2018-02-10 01:52:47 +0100

    Added death and respawn logic

---
+++ Assets/DeathZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+	public RoundManager roundManager;
+
+	void OnTriggerEnter(Collider other)
+	{
+		print(other);
+		print(other.gameObject);
+		roundManager.PlayerDeath(other.GetComponent<Player>());
+	}
+}
---
+++ Assets/Scripts/RoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundManager : MonoBehaviour 
+{
+	public float secondsTillRespawn = 5;
+	public int[] playerScores;
+	public Text playerScoreText;
+	public Transform[] spawnPositions;
+	
+	private Player[] players;
+
+	void Start()
+	{
+		players = FindObjectsOfType<Player>();
+		playerScores = new int[players.Length];
+		foreach(Player player in players)
+		{
+			playerScores[player.id] = 0;
+		}
+	}
+
+	public void PlayerDeath(Player player)
+	{
+		player.gameObject.SetActive(false);
+		StartCoroutine(RespawnCooldown(player));
+	}
+
+	private IEnumerator RespawnCooldown(Player player)
+	{
+		playerScores[player.id]++;
+		yield return new WaitForSeconds(secondsTillRespawn);
+		player.gameObject.SetActive(true);
+		player.transform.position = spawnPositions[player.id].position;
+	}
+}
