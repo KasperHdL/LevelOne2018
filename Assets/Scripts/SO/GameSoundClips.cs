6326e81ed36aea97427583c81ba77d99835076ab
IYavin <itai.yavin@gmail.com>
2018-02-11 10:41:35 +0100

    Playersounds are setup for everything except death. Logic for landing seems broken

---
+++ Assets/Scripts/SO/GameSoundClips.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameSounds", menuName = "", order = 1)]
+public class GameSoundClips : ScriptableObject {
+	public AudioClip[] playerHit;
+	public AudioClip[] dash;
+	public AudioClip[] jump;
+	public AudioClip[] land;
+	public AudioClip[] push;
+	public AudioClip[] dashCooldown;
+	public AudioClip[] pushCooldown;
+}
