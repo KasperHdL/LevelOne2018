5bd263ada1f94c7b3821da20b5fd2f2a39675700
Unknown <mdhe@itu.dk>
2018-02-11 12:09:45 +0100

    Added sceneloadmanager

---
+++ Assets/LoadScenesAdditively.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenesAdditively : MonoBehaviour 
+{
+	public int[] scenesToLoad;
+
+	private void Start()
+	{
+		foreach (int index in scenesToLoad)
+		{
+			print (SceneManager.sceneCount);
+			Scene scene = SceneManager.GetSceneByBuildIndex(index);
+			int numScenes = SceneManager.sceneCount;
+
+			if (scene != null && !scene.isLoaded)
+			{
+				SceneManager.LoadScene(scene.buildIndex, LoadSceneMode.Additive);
+			}
+		}
+	} 
+}
