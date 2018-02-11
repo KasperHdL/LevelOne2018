9f2dd84a1e0a1002f77a214274d262db5c6c421e
Mathias Dam Hedelund <mdhe@itu.dk>
2018-02-11 13:23:15 +0100

    Added swing to logo

---
+++ Assets/Swing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour 
+{
+	private Rigidbody rb;
+	public float forceMultiplier;
+	public float speedMultiplier;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 force = new Vector3(Mathf.Sin(Time.time * speedMultiplier), 0, 0) * forceMultiplier;
+		rb.AddRelativeTorque(force);
+	}
+}
